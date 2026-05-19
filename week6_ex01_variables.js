8cd27aed1cfaa01d205d6b0f25d94037a22ddb37
Siphosethu Dyan <siphosexistence@gmail.com>
2026-05-19 22:12:46 +0200

    Add Exercise 01 - Variables and String Manipulation

---
+++ week6_ex01_variables.js
@@ -0,0 +1,17 @@
+//Declaration of firstName
+let firstName = "Siphosethu";
+
+//Declaration of lastName
+let lastName = "Dyan";
+
+//Declaration of age
+let age = 21;
+
+//Declaration of fullName
+let fullName = `${firstName} ${lastName}`;
+
+//Updating the age variable
+age = 22;
+
+//Printing a greeting to the console
+console.log(`Welcome ${fullName}, ${age} years old.`);
