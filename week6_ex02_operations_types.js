0a59ef84c440bc86accebf336f3f3ab552765c61
Siphosethu Dyan <siphosexistence@gmail.com>
2026-05-20 21:29:39 +0200

    Add Exercise 02 - Number Operations and Type Conversion

---
+++ week6_ex02_operations_types.js
@@ -0,0 +1,23 @@
+//Declaration of Number variable
+let number1 = 365;
+
+//Declaration of Number variable
+let number2 = 4;
+
+//Converting variable Number1 to a string variable
+let numStr = `${number1}`;
+
+//Adding the values of Number variables and storing the value
+let sum = number1 + number2;
+
+//Multiplying the values of Number varibales and storing the value
+let product = number1 * number2;
+
+//Displaying the string variable value to the console
+console.log(numStr);
+
+//Displaying the result of a sum of two numbers
+console.log(`The sum of ${number1} and ${number2} is ${sum}.`);
+
+//Displaying the result of a product of two numbers
+console.log(`The product of ${number1} and ${number2} is ${product}.`);
