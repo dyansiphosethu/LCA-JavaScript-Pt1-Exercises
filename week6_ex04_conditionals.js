c4ca89a1ed8c254625e883f3f16a23a0441a7e31
Siphosethu Dyan <siphosexistence@gmail.com>
2026-05-20 23:58:40 +0200

    Add Exercise 04 - Conditional Logic

---
+++ week6_ex04_conditionals.js
@@ -0,0 +1,14 @@
+//Create a Number variable
+let score = 67;
+
+//Create a Number variable
+let passingGrade = 60;
+
+//Checking if score is greater than or equal to passingGrade
+if (score == passingGrade) {
+  console.log("Perfect");
+} else if (score >= passingGrade) {
+  console.log("Passed");
+} else if (score < passingGrade) {
+  console.log("Failed");
+}
